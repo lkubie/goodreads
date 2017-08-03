ef4d94a9f9f6f9788387a1991ba1ed75ae73ad58
lkubie <lenore.kubi@gmail.com>
2017-08-03 15:48:49 -0600

    added get_review_statistics_from_isbsn

--- lib/goodreads/client/books.rb
+++ lib/goodreads/client/books.rb
@@ -1,6 +1,6 @@
 module Goodreads
   module Books
-    # Search for books
+    # Search for books - note: can't use OAuth for this method currently.
     #
     # query   - Text to match against book title, author, and ISBN fields.
     # options - Optional search parameters
@@ -8,58 +8,45 @@ module Goodreads
     # options[:page] - Which page to returns (default: 1)
     # options[:field] - Search field. One of: title, author, or genre (default is all)
     #
-    def search_books(query, params = {}, oauth = true)
+    def search_books(query, params = {})
       params[:q] = query.to_s.strip
-	  if oauth
-		  data = oauth_request("/search/index", params)
-	  else
-		  data = request("/search/index", params)
-	  end
-      
+	  data = request("/search/index", params)
       Hashie::Mash.new(data["search"])
     end
 
-    # Get book details by Goodreads book ID
+    # Get book details by Goodreads book ID - note: can't use OAuth for this method currently.
     #
-    def book(id, oauth = true)
-		if oauth
-			data = oauth_request("/book/show", id: id)
-		else
-			data = request("/book/show", id: id)
-		end
-		
+    def book(id)
+		data = request("/book/show", id: id)
 		Hashie::Mash.new(data["book"])
     end
 
-    # Get book details by book ISBN
+    # Get book details by book ISBN - note: can't use OAuth for this method currently.
     #
-    def book_by_isbn(isbn, oauth = true)
-		if oauth
-			data = oauth_request("/book/isbn", isbn: isbn)
-		else
-			data = request("/book/isbn", isbn: isbn)
-		end
+    def book_by_isbn(isbn)
+		data = request("/book/isbn", isbn: isbn)
 		Hashie::Mash.new(data["book"])
     end
 
-    # Get book details by book title
+    # Get book details by book title - note: can't use OAuth for this method currently.
     #
-    def book_by_title(title, oauth = true)
-		if oauth
-			data = oauth_request("/book/title", title: title)
-		else
-			data = request("/book/title", title: title)
-		end
-		
+    def book_by_title(title)
+		data = request("/book/title", title: title)
 		Hashie::Mash.new(data["book"])
     end
-	def get_work_id(book_id_list, oauth = true)
-		if oauth
-			data = oauth_request("/book/id_to_work_id", id: book_id_list)
-		else
-			data = request("/book/id_to_work_id", id: book_id_list)
-		end
-		
+	
+	#Get Goodreads work id for a given book ID - note: can't use OAuth for this method currently.
+	#
+	def get_work_id(book_id_list)
+		data = request("/book/id_to_work_id", id: book_id_list)
+		Hashie::Mash.new(data)
+	end
+	
+	#Get Review Statistics given a list of ISBNs
+	#
+	def get_review_statistics_from_isbn(isbns)
+		#note, isbns can be a single isbn or a string of isbns seperated by commans ex: "111111111,2222222222,3333333333"
+		data = request("/review_counts.json", isbns: isbns)
 		Hashie::Mash.new(data)
 	end
   end
