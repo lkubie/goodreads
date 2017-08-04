e2f797d464f4e38f229f53a7265501c0799d52b6
lkubie <lenore.kubi@gmail.com>
2017-08-04 10:48:34 -0600

    added follow and unfollow user functions in Users class

--- lib/goodreads/client/users.rb
+++ lib/goodreads/client/users.rb
@@ -1,10 +1,26 @@
 module Goodreads
-  module Users
-    # Get user details
-    #
-    def user(id)
-      data = request("/user/show", id: id)
-      Hashie::Mash.new(data["user"])
-    end
-  end
+	module Users
+		# Get user details - Note: you cannot use OAuth for this call.
+		#
+		def user(id, oauth = true)
+			data = request("/user/show", id: id)
+			Hashie::Mash.new(data["user"])
+		end
+	
+	
+		# Follow a user - Note: this is an OAuth *ONLY* call
+		#
+		def follow_user(id)
+			options = {"format" => "xml"}
+			data = oauth_request("/user/" + id.to_s + "/followers", options, "post")
+		end
+	
+		# Unfollow a user - Note: this is an OAuth *ONLY* call
+		#
+		def unfollow_user(id)
+			options = {}
+			data = oauth_request("/user/" + id.to_s + "/followers/stop_following.xml", options, "delete")
+		end
+	
+	end
 end
